41d97188ea5b2fb40e847c406fddfeb31839ac90
Twofc-86 <75830886+Twofc-86@users.noreply.github.com>
2023-03-13 11:53:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,464 @@
-const app = document.querySelector('.weather-app');
-const temp = document.querySelector('.temp');
-const dateOutput = document.querySelector('.date');
-const timeOutput = document.querySelector('.time');
-const conditionOutput = document.querySelector('.condition');
-const nameOutput = document.querySelector('.name');
-const icon = document.querySelector('.icon');
-const windOutput = document.querySelector('.wind');
-const cloudOutput = document.querySelector('.cloud');
-const humidityOutput = document.querySelector('.humidity');
-const form = document.querySelector('.locationInput');
-const search = document.querySelector('.search');
-const btn = document.querySelector('.submit');
-const cities = document.querySelectorAll('.city');
+const temp = document.getElementById("temp"),
+    date = document.getElementById("date-time"),
+    condition = document.getElementById("condition"),
+    rain = document.getElementById("rain"),
+    mainIcon = document.getElementById("icon"),
+    currentLocation = document.getElementById("location"),
+    uvIndex = document.querySelector(".uv-index"),
+    uvText = document.querySelector(".uv-text"),
+    windSpeed = document.querySelector(".wind-speed"),
+    sunRise = document.querySelector(".sun-rise"),
+    sunSet = document.querySelector(".sun-set"),
+    humidity = document.querySelector(".humidity"),
+    visibilty = document.querySelector(".visibilty"),
+    humidityStatus = document.querySelector(".humidity-status"),
+    airQuality = document.querySelector(".air-quality"),
+    airQualityStatus = document.querySelector(".air-quality-status"),
+    visibilityStatus = document.querySelector(".visibilty-status"),
+    searchForm = document.querySelector("#search"),
+    search = document.querySelector("#query"),
+    celciusBtn = document.querySelector(".celcius"),
+    fahrenheitBtn = document.querySelector(".fahrenheit"),
+    tempUnit = document.querySelectorAll(".temp-unit"),
+    hourlyBtn = document.querySelector(".hourly"),
+    weekBtn = document.querySelector(".week"),
+    weatherCards = document.querySelector("#weather-cards");
 
-let cityInput = "London";
+let currentCity = "";
+let currentUnit = "c";
+let hourlyorWeek = "week";
 
-cities.forEach((city) => {
-    city.addEventListener('click', (e) => {
-        cityInput = e.target.innerHTML;
-        fetchWeatherData();
-        app.style.opacity = "0";
-    });
-})
+// function to get date and time
+function getDateTime() {
+    let now = new Date(),
+        hour = now.getHours(),
+        minute = now.getMinutes();
 
-
-form.addEventListener('submit', (e) => {
-    if (search.value.length == 0) {
-        alert('please input the name of the city');
-    } else {
-        cityInput = search.value;
-        fetchWeatherData();
-        search.value = "";
-        app.style.opacity = "0";
-    }
-    e.preventDefault();
-});
-
-
-function dayOfTheWeek(day, month, year) {
-    const weekday = [
+    let days = [
         "Sunday",
         "Monday",
         "Tuesday",
         "Wednesday",
         "Thursday",
         "Friday",
-        "Saturday"
+        "Saturday",
     ];
-    return weekday[new Date('${day}/${month}/${year}').getDay()];
-};
+    // 12 hours format
+    hour = hour % 12;
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    let dayString = days[now.getDay()];
+    return `${dayString}, ${hour}:${minute}`;
+}
 
-function fetchWeatherData() {
-    fetch('http://api.weatherapi.com/v1/current.json?key=b48abd75990a47c29c783918230103&q=London&aqi=yes')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            temp.innerHTML = data.current.temp_c + "$#176;";
-            conditionOutput.innerHTML = data.current.condition.text;
-            const date = data.location.localtime;
-            const y = parseInt(date.subtr(0, 4));
-            const m = parseInt(date.subtr(5, 2));
-            const d = parseInt(date.subtr(8, 2));
-            const time = date.subtr(11);
+//Updating date and time
+date.innerText = getDateTime();
+setInterval(() => {
+    date.innerText = getDateTime();
+}, 1000);
 
-            dateOutput.innerHTML = `${dayOfTheWeek(d,m,y)} ${d}, ${m}, ${y}`;
-            timeOutput.innerHTML = time;
-            nameOutput.innerHTML = date.location.name;
-            const iconId = data.current.condition.icon.subtr(
-                "//cdn.weatherapi.com/weather/64x64/".length);
-            icon.src = "./icons/" + iconId;
-
-            cloudOutput.innerHTML = data.current.cloud + "%";
-            humidityOutput.innerHTML = data.current.humidity + "%";
-            windOutput.innerHTML = data.current.wind_kph + "km/h";
-
-            let timeofDay = "day";
-            const code = data.current.condition.code;
-
-            if (!data.current.is_day) {
-                timeofDay = "night";
-            }
-
-            if (code == 1000) {
-                app.style.backgroundImage = 'url(./images/${timeofDay}/clearInterval.jpg)';
-                btn.style.background = "#e5ba92";
-
-                if (timeofDay == "night") {
-                    btn.style.background = "#181e27";
-                }
-
-            } else if (
-                code == 1003 || code == 1006 || code == 1009 || code == 1030 || code == 1069 || code == 1087 || code == 1135 || code == 1237 || code == 1276 || code == 1279 || code == 1282
-            ) {
-                app.style.backgroundImage = 'url(./images/$(timeofDay)/cloudy.jpg)';
-                btn.style.background = "#fa6d1b";
-                if (timeofdat == "night") {
-                    btn.style.background = "#181e27";
-                }
-            } else if (
-                code == 1063 || code == 1069 || code == 1072 || code == 1150 || code == 1153 || code == 1180 || code == 1183 || code == 1186 || code == 1189 || code == 1192 || code == 1195 || code == 1204 || code == 1207 || code == 1240 || code == 1243 || code == 1246 || code == 1249 || code == 1252
-            ) {
-                app.style.backgroundImage = 'url(./images/${timeOfDay}/rainy.jpg';
-                btn.style.background = "#647d75";
-                if (timeofDay == "night") {
-                    btn.style.background = "#325c80";
-                }
-            } else {
-                app.style.backgroundImage = 'url(./images/$(timeofDay)/snowy.jpg)'
-                btn.style.background = "#4d72aa";
-                if (timeofDay == "night") {
-                    btn.style.background = '#1b1b1b';
-                }
-            }
-            app.style.opacity = "1";
+// function to get public ip address
+function getPublicIp() {
+    fetch("https://geolocation-db.com/json/", {
+            method: "GET",
+            headers: {},
         })
-        .catch(() => {
-            alert('city not found, please try another city or try again');
-
+        .then((response) => response.json())
+        .then((data) => {
+            currentCity = data.city;
+            getWeatherData(data.city, currentUnit, hourlyorWeek);
+        })
+        .catch((err) => {
+            console.error(err);
         });
 }
 
-fetchWeatherData();
+getPublicIp();
+
+// function to get weather data
+function getWeatherData(city, unit, hourlyorWeek) {
+    fetch(
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=T5VY9TGHR67HGL7VFC7656X6L&contentType=json`, {
+                method: "GET",
+                headers: {},
+            }
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            let today = data.currentConditions;
+            if (unit === "c") {
+                temp.innerText = today.temp;
+            } else {
+                temp.innerText = celciusToFahrenheit(today.temp);
+            }
+            currentLocation.innerText = data.resolvedAddress;
+            condition.innerText = today.conditions;
+            rain.innerText = "Perc - " + today.precip + "%";
+            uvIndex.innerText = today.uvindex;
+            windSpeed.innerText = today.windspeed;
+            measureUvIndex(today.uvindex);
+            mainIcon.src = getIcon(today.icon);
+            changeBackground(today.icon);
+            humidity.innerText = today.humidity + "%";
+            updateHumidityStatus(today.humidity);
+            visibilty.innerText = today.visibility;
+            updateVisibiltyStatus(today.visibility);
+            airQuality.innerText = today.winddir;
+            updateAirQualityStatus(today.winddir);
+            if (hourlyorWeek === "hourly") {
+                updateForecast(data.days[0].hours, unit, "day");
+            } else {
+                updateForecast(data.days, unit, "week");
+            }
+            sunRise.innerText = covertTimeTo12HourFormat(today.sunrise);
+            sunSet.innerText = covertTimeTo12HourFormat(today.sunset);
+        })
+        .catch((err) => {
+            alert("City not found in our database");
+        });
+}
+
+//function to update Forecast
+function updateForecast(data, unit, type) {
+    weatherCards.innerHTML = "";
+    let day = 0;
+    let numCards = 0;
+    if (type === "day") {
+        numCards = 24;
+    } else {
+        numCards = 7;
+    }
+    for (let i = 0; i < numCards; i++) {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let dayName = getHour(data[day].datetime);
+        if (type === "week") {
+            dayName = getDayName(data[day].datetime);
+        }
+        let dayTemp = data[day].temp;
+        if (unit === "f") {
+            dayTemp = celciusToFahrenheit(data[day].temp);
+        }
+        let iconCondition = data[day].icon;
+        let iconSrc = getIcon(iconCondition);
+        let tempUnit = "°C";
+        if (unit === "f") {
+            tempUnit = "°F";
+        }
+        card.innerHTML = `
+                <h2 class="day-name">${dayName}</h2>
+            <div class="card-icon">
+              <img src="${iconSrc}" class="day-icon" alt="" />
+            </div>
+            <div class="day-temp">
+              <h2 class="temp">${dayTemp}</h2>
+              <span class="temp-unit">${tempUnit}</span>
+            </div>
+  `;
+        weatherCards.appendChild(card);
+        day++;
+    }
+}
+
+// function to change weather icons
+function getIcon(condition) {
+    if (condition === "partly-cloudy-day") {
+        return "https://i.ibb.co/PZQXH8V/27.png";
+    } else if (condition === "partly-cloudy-night") {
+        return "https://i.ibb.co/Kzkk59k/15.png";
+    } else if (condition === "rain") {
+        return "https://i.ibb.co/kBd2NTS/39.png";
+    } else if (condition === "clear-day") {
+        return "https://i.ibb.co/rb4rrJL/26.png";
+    } else if (condition === "clear-night") {
+        return "https://i.ibb.co/1nxNGHL/10.png";
+    } else {
+        return "https://i.ibb.co/rb4rrJL/26.png";
+    }
+}
+
+// function to change background depending on weather conditions
+function changeBackground(condition) {
+    const body = document.querySelector(".back-bg");
+    let bg = "";
+    if (condition === "partly-cloudy-day") {
+        bg = "images/day/cloudy.jpg";
+    } else if (condition === "partly-cloudy-night") {
+        bg = "images/night/cloudy.jpg";
+    } else if (condition === "rain") {
+        bg = "images/night/rainy.jpg";
+    } else if (condition === "clear-day") {
+        bg = "images/day/clearly.jpg";
+    } else if (condition === "clear-night") {
+        bg = "images/night/clearly.jpg";
+    } else {
+        bg = "images/day/clearly.jpg";
+    }
+    body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${bg})`;
+}
+
+//get hours from hh:mm:ss
+function getHour(time) {
+    let hour = time.split(":")[0];
+    let min = time.split(":")[1];
+    if (hour > 12) {
+        hour = hour - 12;
+        return `${hour}:${min} PM`;
+    } else {
+        return `${hour}:${min} AM`;
+    }
+}
+
+// convert time to 12 hour format
+function covertTimeTo12HourFormat(time) {
+    let hour = time.split(":")[0];
+    let minute = time.split(":")[1];
+    let ampm = hour >= 12 ? "pm" : "am";
+    hour = hour % 12;
+    hour = hour ? hour : 12; // the hour '0' should be '12'
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    let strTime = hour + ":" + minute + " " + ampm;
+    return strTime;
+}
+
+// function to get day name from date
+function getDayName(date) {
+    let day = new Date(date);
+    let days = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+    ];
+    return days[day.getDay()];
+}
+
+// function to get uv index status
+function measureUvIndex(uvIndex) {
+    if (uvIndex <= 2) {
+        uvText.innerText = "Low";
+    } else if (uvIndex <= 5) {
+        uvText.innerText = "Normal";
+    } else if (uvIndex <= 7) {
+        uvText.innerText = "High";
+    } else if (uvIndex <= 10) {
+        uvText.innerText = "Very High";
+    } else {
+        uvText.innerText = "Extreme";
+    }
+}
+
+// function to get humidity status
+function updateHumidityStatus(humidity) {
+    if (humidity <= 30) {
+        humidityStatus.innerText = "Low";
+    } else if (humidity <= 60) {
+        humidityStatus.innerText = "Normal";
+    } else {
+        humidityStatus.innerText = "High";
+    }
+}
+
+// function to get visibility status
+function updateVisibiltyStatus(visibility) {
+    if (visibility <= 0.03) {
+        visibilityStatus.innerText = "Dense Fog";
+    } else if (visibility <= 0.16) {
+        visibilityStatus.innerText = "Moderate Fog";
+    } else if (visibility <= 0.35) {
+        visibilityStatus.innerText = "Light Fog";
+    } else if (visibility <= 1.13) {
+        visibilityStatus.innerText = "Very Light Fog";
+    } else if (visibility <= 2.16) {
+        visibilityStatus.innerText = "Light Mist";
+    } else if (visibility <= 5.4) {
+        visibilityStatus.innerText = "Very Light Mist";
+    } else if (visibility <= 10.8) {
+        visibilityStatus.innerText = "Clear Air";
+    } else {
+        visibilityStatus.innerText = "Very Clear Air";
+    }
+}
+
+// function to get air quality status
+function updateAirQualityStatus(airquality) {
+    if (airquality <= 50) {
+        airQualityStatus.innerText = "Safe";
+    } else if (airquality <= 100) {
+        airQualityStatus.innerText = "Normal";
+    } else if (airquality <= 150) {
+        airQualityStatus.innerText = "Slighty Risk";
+    } else if (airquality <= 200) {
+        airQualityStatus.innerText = "Unhealthy";
+    } else if (airquality <= 250) {
+        airQualityStatus.innerText = "Very Unhealthy";
+    } else {
+        airQualityStatus.innerText = "Hazardous";
+    }
+}
+
+// function to handle search form
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let location = search.value;
+    if (location) {
+        currentCity = location;
+        getWeatherData(location, currentUnit, hourlyorWeek);
+    }
+});
+
+// function to conver celcius to fahrenheit
+function celciusToFahrenheit(temp) {
+    return ((temp * 9) / 5 + 32).toFixed(1);
+}
+
+
+var currentFocus;
+search.addEventListener("input", function(e) {
+    removeSuggestions();
+    var a,
+        b,
+        i,
+        val = this.value;
+    if (!val) {
+        return false;
+    }
+    currentFocus = -1;
+
+    a = document.createElement("ul");
+    a.setAttribute("id", "suggestions");
+
+    this.parentNode.appendChild(a);
+
+    for (i = 0; i < cities.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (
+            cities[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()
+        ) {
+            /*create a li element for each matching element:*/
+            b = document.createElement("li");
+            /*make the matching letters bold:*/
+            b.innerHTML =
+                "<strong>" + cities[i].name.substr(0, val.length) + "</strong>";
+            b.innerHTML += cities[i].name.substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + cities[i].name + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                search.value = this.getElementsByTagName("input")[0].value;
+                removeSuggestions();
+            });
+
+            a.appendChild(b);
+        }
+    }
+});
+
+/*execute a function presses a key on the keyboard:*/
+search.addEventListener("keydown", function(e) {
+    var x = document.getElementById("suggestions");
+    if (x) x = x.getElementsByTagName("li");
+    if (e.keyCode == 40) {
+        /*If the arrow DOWN key
+          is pressed,
+          increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+    } else if (e.keyCode == 38) {
+        /*If the arrow UP key
+          is pressed,
+          decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+    }
+    if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+        }
+    }
+});
+
+function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = x.length - 1;
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("active");
+}
+
+function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("active");
+    }
+}
+
+function removeSuggestions() {
+    var x = document.getElementById("suggestions");
+    if (x) x.parentNode.removeChild(x);
+}
+
+fahrenheitBtn.addEventListener("click", () => {
+    changeUnit("f");
+});
+celciusBtn.addEventListener("click", () => {
+    changeUnit("c");
+});
+
+// function to change unit
+function changeUnit(unit) {
+    if (currentUnit !== unit) {
+        currentUnit = unit;
+        tempUnit.forEach((elem) => {
+            elem.innerText = `°${unit.toUpperCase()}`;
+        });
+        if (unit === "c") {
+            celciusBtn.classList.add("active");
+            fahrenheitBtn.classList.remove("active");
+        } else {
+            celciusBtn.classList.remove("active");
+            fahrenheitBtn.classList.add("active");
+        }
+        getWeatherData(currentCity, currentUnit, hourlyorWeek);
+    }
+}
+
+hourlyBtn.addEventListener("click", () => {
+    changeTimeSpan("hourly");
+});
+weekBtn.addEventListener("click", () => {
+    changeTimeSpan("week");
+});
+
+
+// function to change hourly to weekly or vice versa
+function changeTimeSpan(unit) {
+    if (hourlyorWeek !== unit) {
+        hourlyorWeek = unit;
+        if (unit === "hourly") {
+            hourlyBtn.classList.add("active");
+            weekBtn.classList.remove("active");
+        } else {
+            hourlyBtn.classList.remove("active");
+            weekBtn.classList.add("active");
+        }
+        getWeatherData(currentCity, currentUnit, hourlyorWeek);
+    }
+}
